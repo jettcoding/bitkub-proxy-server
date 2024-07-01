4823f7e5085816803b96d9f4983508aaf0ff516a
TIRAJET W. <tirajetw@gmail.com>
2024-07-01 22:41:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
