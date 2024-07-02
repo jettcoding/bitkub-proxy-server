9d10af7ca2570843ae707744494e6971045d349e
TIRAJET W. <tirajetw@gmail.com>
2024-07-02 21:39:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
